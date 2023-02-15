9163e49d1d325126ec7466d636e04b266bedaa9c
Seregey_Maximov <dev9092336062@gmail.com>
2023-02-15 17:37:25 +0300

    header links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,38 @@
 import styles from './styles.module.scss'
+import {BASE_URL} from "../../api/baseURL";
+import {NavLink} from "react-router-dom";
 
-const Header = () =>{
+const Header = () => {
   return (
       <header className={styles.header}>
-        header
+        <ul className={styles.header__list}>
+          <li>
+            <NavLink
+                to={`${BASE_URL}/`}
+                className={styles.header__link}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+                to={`${BASE_URL}/projects`}
+                className={styles.header__link}
+            >
+              Projects
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+                to={`${BASE_URL}/contacts`}
+                className={styles.header__link}
+            >
+              Contacts
+            </NavLink>
+          </li>
+        </ul>
       </header>
   )
 }
