585063c944b004653616dab0dabf5aa3194eca76
Seregey_Maximov <dev9092336062@gmail.com>
2023-02-15 17:48:12 +0300

    files

--- src/ layouts/baseLayout/index.tsx
+++ src/ layouts/baseLayout/index.tsx
@@ -1,12 +1,19 @@
 import {FC} from "react";
 import Header from "../../components/header";
 import {IBaseLayout} from "./types";
+import {useNavigation} from "react-router-dom";
+import styles from "./styles.module.scss";
 
 const BaseLayout: FC<IBaseLayout> = ({children}) => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading"
+
   return (
       <>
         <Header/>
-        {children}
+        <div className={styles.contentLayout}>
+          {children}
+        </div>
       </>
   )
 }
