9299815ee8d579f8cca961879dd2026e47e58d7d
Seregey_Maximov <dev9092336062@gmail.com>
2023-02-20 16:37:19 +0300

    import constants in module.scss

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-2023"
+  base: "/Portfolio-2023",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/assets/styles/constants.scss";`
+      }
+    }
+  },
 })
+
+
