0b6a2962b83ef90e81af14eebfc434f3556cffa7
Seregey_Maximov <dev9092336062@gmail.com>
2023-02-27 12:21:15 +0300

    home content

--- src/components/home-content/index.tsx
+++ src/components/home-content/index.tsx
@@ -5,10 +5,17 @@ import {LangEnum} from "../../constants/enums";
 const HomeContent = () => {
   const {lang} = useContext(Context)
 
+  const result = []
+
+  for (let i = 0; i < 100; i++){
+    lang === LangEnum.EN && result.push('Page home under construction')
+    lang === LangEnum.RU && result.push('Главная страница в разработке')
+    result.push(<br/>)
+  }
+
   return (
       <>
-        {lang === LangEnum.EN && 'Page home under construction'}
-        {lang === LangEnum.RU && 'Главная страница в разработке'}
+        {result}
       </>
   );
 }
