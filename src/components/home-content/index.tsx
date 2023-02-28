2b3935e7dd21e40b9856880d78e9b4ba8a232242
Seregey_Maximov <dev9092336062@gmail.com>
2023-02-28 14:32:13 +0300

    home content - structure

--- src/components/home-content/index.tsx
+++ src/components/home-content/index.tsx
@@ -1,27 +1,57 @@
 import {useContext} from "react";
 import {Context} from "../../context/context";
 import {LangEnum} from "../../constants/enums";
+import styles from "./styles.module.scss"
+import classNames from "classnames";
 
 const HomeContent = () => {
   const {lang} = useContext(Context)
 
-  const result = []
-
-  for (let i = 0; i < 100; i++) {
-    const node =
-        <div key={i}>
-          {lang === LangEnum.EN && 'Page home under construction'}
-          {lang === LangEnum.RU && 'Главная страница в разработке'}
-          <br/>
-        </div>
-    
-    result.push(node)
-  }
-
   return (
-      <>
-        {result}
-      </>
+      <div className={styles.home}>
+        <h1 className={styles.home__title}>
+              <span>
+                {lang === LangEnum.EN && 'Frontend-developer'}
+                {lang === LangEnum.RU && 'Фронтенд разработчик'}
+              </span>
+        </h1>
+
+        <section className={classNames(styles.home__section, styles.about)}>
+          <div className={styles.home__photo}>
+            {/* TODO: photo --------------------------------------------------------------------- */}
+          </div>
+
+          <h2 className={classNames(styles.home__subtitle, styles.about__title)}>
+            {lang === LangEnum.EN && 'Sergey Maximov'}
+            {lang === LangEnum.RU && 'Сергей Максимов'}
+          </h2>
+
+          <p className={classNames(styles.home__text, styles.about__text)}>
+            {lang === LangEnum.EN &&
+                // TODO: EN  ---------------------------------------------------------------------
+                'Занимаюсь разработкой приложений на TS/JS, React, Next. ' +
+                'Имею опыт разработки CRM приложений. ' +
+                'Опыт подготовки адаптивной верстки и анимаций.'
+            }
+            {lang === LangEnum.RU &&
+                'Занимаюсь разработкой приложений на TS/JS, React, Next. ' +
+                'Имею опыт разработки CRM приложений. ' +
+                'Опыт подготовки адаптивной верстки и анимаций.'
+            }
+          </p>
+        </section>
+
+        <section className={classNames(styles.home__section, styles.skills)}>
+          <h2 className={classNames(styles.home__subtitle, styles.skills__title)}>
+            {lang === LangEnum.EN && 'Skills'}
+            {lang === LangEnum.RU && 'Навыки'}
+          </h2>
+
+          <div className={styles.skills__content}>
+            {/* TODO: skills ---------------------------------------------------------------------- */}
+          </div>
+        </section>
+      </div>
   );
 }
 
