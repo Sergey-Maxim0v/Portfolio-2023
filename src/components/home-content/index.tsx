7cff930385d1f6a91043be4d57db26af8ef6e97f
Seregey_Maximov <dev9092336062@gmail.com>
2023-03-01 12:40:21 +0300

    home text EN

--- src/components/home-content/index.tsx
+++ src/components/home-content/index.tsx
@@ -8,6 +8,7 @@ const HomeContent = () => {
   const {lang} = useContext(Context)
 
   const downloadResume = (lang: LangEnum) => {
+    // TODO
     console.log(lang)
   }
 
@@ -39,11 +40,11 @@ const HomeContent = () => {
             {lang === LangEnum.EN &&
                 'I like to resolve design problems, ' +
                 'create smart user interface and imagine useful interaction, ' +
-                'developing rich web experiences & web applications.'
+                'developing rich web experiences & web applications. '
             }
             {lang === LangEnum.RU &&
-                'Мне нравится решать сложные дизайнерские задачи, ' +
-                'разрабатывать интересный функционал для web-приложений.'
+                'Мне нравится решать сложные дизайнерские задачи ' +
+                'и разрабатывать интересный функционал для web-приложений. '
             }
           </p>
         </section>
@@ -65,7 +66,83 @@ const HomeContent = () => {
           </h2>
 
           <div className={styles.skills__content}>
-            {/* TODO: skills ---------------------------------------------------------------------- */}
+            <div className={styles.skills__element}>
+              <h4 className={styles.skills__subtitle}>
+                TypeScrip, JavaScript
+              </h4>
+              <p className={styles.skills__description}>
+                {lang === LangEnum.EN &&
+                    'Knowledge of the capabilities and features of programming languages, ' +
+                    'extensive experience with DOM and events,' +
+                    'communication between the client and the server. ' +
+                    'Knowledge and experience in applying basic programming patterns. ' +
+                    'Knowledge of browser features ' +
+                    'and the principles of creating modern large Web applications. '
+                }
+                {lang === LangEnum.RU &&
+                    'Знание возможностей и особенностей языков, ' +
+                    'в т.ч. большой опыт работы с DOM и событиями, ' +
+                    'общением клиента с сервером. ' +
+                    'Знание и опыт применения основных паттернов программирования. ' +
+                    'Знание особенностей работы браузеров ' +
+                    'и принципов создания современных крупных Web-приложений. '
+                }
+              </p>
+            </div>
+
+            <div className={styles.skills__element}>
+              <h4 className={styles.skills__subtitle}>
+                React, Next.js
+              </h4>
+              <p className={styles.skills__description}>
+                {lang === LangEnum.EN &&
+                    'Experience in creating React applications of varying degrees of complexity. ' +
+                    'Experience in creating SSR applications using Next.js ' +
+                    'Experience working with React libraries for building and interacting with tables and forms. ' +
+                    'Experience working with React UI libraries (Material UI).'
+                }
+                {lang === LangEnum.RU &&
+                    'Опыт создания приложений на React разной степени сложности. ' +
+                    'Опыт создания SSR приложений с использованием Next.js. ' +
+                    'Опыт работы с React библиотеками работы с таблицами, формами. ' +
+                    'Опыт работы с  React библиотеками UI (Material UI). '
+                }
+              </p>
+            </div>
+
+            <div className={styles.skills__element}>
+              <h4 className={styles.skills__subtitle}>
+                HTML, CSS, SASS, Tailwind, Nunjucks
+              </h4>
+              <p className={styles.skills__description}>
+                {lang === LangEnum.EN &&
+                    'Experience of layout based on layouts of varying complexity. ' +
+                    'Experience writing animations. ' +
+                    'Knowledge of CSS frameworks.'
+                }
+                {lang === LangEnum.RU &&
+                    'Опыт верстки на основе макетов разной сложности. ' +
+                    'Опыт написания анимаций. ' +
+                    'Знание CSS фреймворков. '
+                }
+              </p>
+            </div>
+
+            <div className={styles.skills__element}>
+              <h4 className={styles.skills__subtitle}>
+                Environments
+              </h4>
+              <p className={styles.skills__description}>
+                {lang === LangEnum.EN &&
+                    'Experience working with tools used in frontend development:: ' +
+                    'Git, Yarn, NPM, Webpack, Vite, Eslint'
+                }
+                {lang === LangEnum.RU &&
+                    'Опыт работы с инструментами применяемыми во frontend разработке: ' +
+                    'Git, Yarn, NPM, Webpack, Vite, Eslint'
+                }
+              </p>
+            </div>
           </div>
         </section>
       </div>
