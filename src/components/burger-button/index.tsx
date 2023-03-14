f786d96276821ea1940dcc5bbab82677a777ede8
Seregey_Maximov <dev9092336062@gmail.com>
2023-03-14 18:59:04 +0300

    burger todo

--- src/components/burger-button/index.tsx
+++ src/components/burger-button/index.tsx
@@ -1,13 +1,28 @@
 import styles from './styles.module.scss'
-import {FC} from "react";
+import {FC, useState, MouseEvent} from "react";
 import classNames from "classnames";
 import {IBurgerButton} from "./types";
 
-const BurgerButton: FC<IBurgerButton> = ({className}) => {
+const BurgerButton: FC<IBurgerButton> = ({className, callBack}) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const onClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    setIsActive(prev => !prev)
+    // callBack()
+  }
+
+  // TODO: https://codepen.io/brenden/pen/VLjKMQ
+
   return (
-      <div className={classNames(styles.burger, className)}>
-        // TODO
-      </div>
+      <button
+          className={classNames(styles.burger, className, {[styles.active]: isActive})}
+          onClick={(event) => onClick(event)}
+      >
+        <div className={classNames(styles.line1, styles.line)}></div>
+        <div className={classNames(styles.line2, styles.line)}></div>
+        <div className={classNames(styles.line3, styles.line)}></div>
+      </button>
   )
 }
 
