69f5148b099aa111cf3db09fbf9e53477fea34dc
Sergey Maximov <dev9092336062@gmail.com>
2023-04-28 20:51:58 +0300

    PROJECTS_LIST ru

--- src/constants/projects-list.tsx
+++ src/constants/projects-list.tsx
@@ -14,27 +14,31 @@ export interface IProject {
 export const PROJECTS_LIST: IProject[] = [
     {
         id: 'project__zena',
-        title: 'Zena',
+        title: 'Zen-Auto',
         photos: [],
-        developers: '4 - 6',
-        time: 7,
-        technologies: '',
-        descriptionRu: '',
-        descriptionEn: '',
-        myWorkRu: '',
-        myWorkEn: ''
+        developers: '4 - 7',
+        time: 10,
+        technologies: 'Next, Typescript, Material-UI',
+        descriptionRu: 'CRM, приложение для учета загруженности сотрудников, ' +
+            'сохранения ежедневных итогов работы сотрудников' +
+            ' и&nbsp;подготовки сводных таблиц и отчетов.',
+        descriptionEn: '// TODO', // TODO: en
+        myWorkRu: 'Пользовательская часть приложения: настройка взаимодействия пользователя ' +
+            'с&nbsp;формами, настройка автосохранений, сохранений, интерактивные таблицы.',
+        myWorkEn: '// TODO ' // TODO: en
     },
     {
         id: 'project__zen-lendos',
-        title: 'ZenCode website',
+        title: 'ZenCode',
         photos: [],
         developers: '1',
         time: 2,
-        technologies: '',
-        descriptionRu: '',
-        descriptionEn: '',
-        myWorkRu: '',
-        myWorkEn: ''
+        technologies: 'Next, Javascript',
+        descriptionRu: 'Лендинг',
+        descriptionEn: '// TODO', // TODO: en
+        myWorkRu: 'Обновление приложения согласно новым макетам, простые анимации, ' +
+            'корректировка логик работы приложения, рефакторинг.',
+        myWorkEn: '// TODO ' // TODO: en
     },
     {
         id: 'project__zen-dock',
@@ -42,11 +46,13 @@ export const PROJECTS_LIST: IProject[] = [
         photos: [],
         developers: '1',
         time: 1,
-        technologies: '',
-        descriptionRu: '',
-        descriptionEn: '',
-        myWorkRu: '',
-        myWorkEn: ''
+        technologies: 'Electron, React, Typescript, Tailwind CSS',
+        descriptionRu: 'Приложение работает локально. Сохраняет архив с&nbsp;бухгалтерскими ' +
+            'документами в&nbsp;формате DOCX, на&nbsp;основании введенных пользователем данных',
+        descriptionEn: '// TODO', // TODO: en
+        myWorkRu: 'Исправление и настройка приложения (на&nbsp;момент начала работы приложение не&nbsp;работало), ' +
+            'корректировка стилей',
+        myWorkEn: '// TODO ' // TODO: en
     },
     {
         id: 'project__rkdr',
@@ -54,11 +60,12 @@ export const PROJECTS_LIST: IProject[] = [
         photos: [],
         developers: '3',
         time: 3,
-        technologies: '',
-        descriptionRu: '',
-        descriptionEn: '',
-        myWorkRu: '',
-        myWorkEn: ''
+        technologies: 'Nunjucks, JS, SCSS, GSAP',
+        descriptionRu: 'Новый сайт на 1C-Bitrix для гос. учереждения',
+        descriptionEn: '// TODO', // TODO: en
+        myWorkRu: 'Подготовка верстки, написание скриптов работы интерактивных элементов ' +
+            '(модалки, аккордеоны, выпадающие меню...), простые анимации',
+        myWorkEn: '// TODO ' // TODO: en
     },
     {
         id: 'project__okna',
@@ -66,10 +73,11 @@ export const PROJECTS_LIST: IProject[] = [
         photos: [],
         developers: '2',
         time: 1,
-        technologies: '',
-        descriptionRu: '',
-        descriptionEn: '',
-        myWorkRu: '',
-        myWorkEn: ''
+        technologies: 'Nunjucks, JS, SCSS, GSAP',
+        descriptionRu: 'Новый сайт на 1C-Bitrix для производителя пластиковых окон',
+        descriptionEn: '// TODO', // TODO: en
+        myWorkRu: 'Подготовка верстки, написание скриптов работы интерактивных элементов ' +
+            '(модалки, аккордеоны, выпадающие меню...), простые анимации',
+        myWorkEn: '// TODO ' // TODO: en
     }
 ]
