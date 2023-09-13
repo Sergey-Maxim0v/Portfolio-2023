92b0e49fe53bfe75dde86fe89ef234a5652cccbc
Sergey Maximov <dev9092336062@gmail.com>
2023-09-13 21:49:42 +0300

    PROJECTS_LIST: fix

--- src/constants/projects-list.tsx
+++ src/constants/projects-list.tsx
@@ -32,11 +32,11 @@ export const PROJECTS_LIST: IProject[] = [
       "save the daily results of employees " +
       "and compiling summary tables and reports.",
     myWorkRu:
-      "работал над пользовательской частью приложения, настроил взаимодействия пользователя " +
-      "с формами, настроил автосохранения, сохранения, написал интерактивные таблицы",
+      "Работал над пользовательской частью приложения, настроил взаимодействия пользователя " +
+      "с формами, настроил автосохранения, сохранения, написал интерактивные таблицы.",
     myWorkEn:
-      "worked on the user part of the application, set up user interaction" +
-      "with forms, configured autosave, saved, wrote interactive tables",
+      "Worked on the user part of the application, set up user interaction" +
+      "with forms, configured autosave, saved, wrote interactive tables.",
   },
   {
     id: "project__zen-lendos",
@@ -50,11 +50,11 @@ export const PROJECTS_LIST: IProject[] = [
     descriptionRu: "Лендинг",
     descriptionEn: "Landing",
     myWorkRu:
-      "обновил приложение согласно новым макетам, написал простые анимации, " +
-      "скорректировал логики работы приложения, рефакторинг",
+      "Обновил приложение согласно новым макетам, написал простые анимации, " +
+      "скорректировал логики работы приложения, рефакторинг.",
     myWorkEn:
-      "updated the application according to the new layouts, wrote simple animations, " +
-      "corrected the logic of the application, refactoring",
+      "Updated the application according to the new layouts, wrote simple animations, " +
+      "corrected the logic of the application, refactoring.",
   },
   {
     id: "project__zen-dock",
@@ -67,19 +67,19 @@ export const PROJECTS_LIST: IProject[] = [
     technologies: "Electron, React, Typescript, Tailwind CSS",
     descriptionRu:
       "Приложение работает локально. Сохраняет архив с бухгалтерскими " +
-      "документами в формате DOCX, на основании введенных пользователем данных",
+      "документами в формате DOCX, на основании введенных пользователем данных.",
     descriptionEn:
       "The application is running locally. " +
       "Saves an archive with accounting documents in DOCX format, " +
-      "based on the data entered by the user",
+      "based on the data entered by the user.",
     myWorkRu:
-      "исправил и настроил приложение " +
+      "Исправил и настроил приложение " +
       "(на момент начала работы приложение падало из-за ошибок), " +
-      "скорректировал стили согласно пожеланиям заказчика",
+      "скорректировал стили согласно пожеланиям заказчика.",
     myWorkEn:
-      "fixed and configured the application " +
+      "Fixed and configured the application " +
       "(at the time of the start of work, the application was crashing due to errors), " +
-      "adjusted the styles according to the wishes of the customer",
+      "adjusted the styles according to the wishes of the customer.",
   },
   {
     id: "project__rkdr",
@@ -93,11 +93,11 @@ export const PROJECTS_LIST: IProject[] = [
     descriptionRu: "Сайт на 1C-Bitrix для гос. учреждения",
     descriptionEn: "Site on 1C-Bitrix for a government agency",
     myWorkRu:
-      "написал верстку, написал скрипты работы интерактивных элементов " +
-      "(модалки, аккордеоны, выпадающие меню...), написал простые анимации",
+      "Написал верстку, написал скрипты работы интерактивных элементов " +
+      "(модалки, аккордеоны, выпадающие меню...), написал простые анимации.",
     myWorkEn:
-      "wrote the layout, wrote scripts for interactive elements " +
-      "(modals, accordions, dropdown menus...), wrote simple animations",
+      "Wrote the layout, wrote scripts for interactive elements " +
+      "(modals, accordions, dropdown menus...), wrote simple animations.",
   },
   {
     id: "project__okna",
@@ -109,15 +109,15 @@ export const PROJECTS_LIST: IProject[] = [
     time: 1,
     technologies: "Nunjucks, JS, SCSS, GSAP",
     descriptionRu:
-      "Обновленный сайт на 1C-Bitrix для производителя пластиковых окон",
+      "Обновленный сайт на 1C-Bitrix для производителя пластиковых окон.",
     descriptionEn:
       "Updated site on 1C-Bitrix for the manufacturer of plastic windows.",
     myWorkRu:
       "Написал верстку, написал скрипты работы интерактивных элементов " +
-      "(модалки, аккордеоны, выпадающие меню...), написал простые анимации",
+      "(модалки, аккордеоны, выпадающие меню...), написал простые анимации.",
     myWorkEn:
       "Wrote layout, wrote scripts for interactive elements " +
-      "(modals, accordions, drop-down menus...), wrote simple animations",
+      "(modals, accordions, drop-down menus...), wrote simple animations.",
   },
   // TODO: скрыто до 01.10.2023
   // {
