aa232c41f392fef839f43d926ece2f7decd1c348
Sergey Maximov <dev9092336062@gmail.com>
2023-11-26 19:05:55 +0300

    useMedia: try catch

--- src/hooks/useMedia.tsx
+++ src/hooks/useMedia.tsx
@@ -11,11 +11,43 @@ const mobileMatchMin = "(min-width: 768px)";
 const tabletMatchMax = "(max-width: 1023px)";
 const desktopMatch = "(min-width: 1024px)";
 
-const getIsMobile = () => window.matchMedia(mobileMatch).matches;
-const getIsTablet = () =>
-  window.matchMedia(mobileMatchMin).matches &&
-  window.matchMedia(tabletMatchMax).matches;
-const getIsDesktop = () => window.matchMedia(desktopMatch).matches;
+const getIsMobile = () => {
+  let isMobile = false;
+
+  try {
+    isMobile = window.matchMedia(mobileMatch).matches;
+  } catch (error) {
+    console.error("getIsMobile:::", error);
+  }
+
+  return isMobile;
+};
+
+const getIsTablet = () => {
+  let isTablet = false;
+
+  try {
+    isTablet =
+      window.matchMedia(mobileMatchMin).matches &&
+      window.matchMedia(tabletMatchMax).matches;
+  } catch (error) {
+    console.error("getIsTablet:::", error);
+  }
+
+  return isTablet;
+};
+
+const getIsDesktop = () => {
+  let isDesktop = false;
+
+  try {
+    isDesktop = window.matchMedia(desktopMatch).matches;
+  } catch (error) {
+    console.error("getIsDesktop:::", error);
+  }
+
+  return isDesktop;
+};
 
 const useMedia = (): IUseMedia => {
   const [isMobile, seIsMobile] = useState(getIsMobile());
