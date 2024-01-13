4406c174969f3c7e26f127df87ff2aeeb423c898
Sergey Maximov <dev9092336062@gmail.com>
2024-01-14 01:45:59 +0300

    slide images: todo

--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -4,6 +4,7 @@ import styles from "./styles.module.scss";
 import classNames from "classnames";
 import { LangEnum } from "../../constants/enums";
 import { Context } from "../../context/context";
+import SlideImages from "../slide-images";
 
 const Slide: FC<ISlide> = ({ project, className }) => {
   const { lang } = useContext(Context);
@@ -13,62 +14,66 @@ const Slide: FC<ISlide> = ({ project, className }) => {
   return (
     <div className={classNames(className, styles.slideRow)}>
       <div className={styles.slide}>
-        <h4 className={styles.title}>
-          {project.title} <span className={styles.date}>{project.date}</span>
-        </h4>
+        <div className={styles.content}>
+          <h4 className={styles.title}>
+            {project.title} <span className={styles.date}>{project.date}</span>
+          </h4>
 
-        <ul className={styles.list}>
-          <li className={styles.element}>
-            <span className={styles.subtitle}>
-              {lang === LangEnum.RU && `Стек технологий:`}
-              {lang === LangEnum.EN && `Technology stack:`}
-            </span>
-
-            {project.technologies}
-          </li>
-
-          {project.descriptionRu && project.descriptionEn ? (
+          <ul className={styles.list}>
             <li className={styles.element}>
               <span className={styles.subtitle}>
-                {lang === LangEnum.RU && `Описание:`}
-                {lang === LangEnum.EN && `Description:`}
+                {lang === LangEnum.RU && `Стек технологий:`}
+                {lang === LangEnum.EN && `Technology stack:`}
               </span>
 
-              {lang === LangEnum.RU && project.descriptionRu}
-              {lang === LangEnum.EN && project.descriptionEn}
+              {project.technologies}
             </li>
-          ) : null}
 
-          {project.purposeRu && project.purposeEn ? (
+            {project.descriptionRu && project.descriptionEn ? (
+              <li className={styles.element}>
+                <span className={styles.subtitle}>
+                  {lang === LangEnum.RU && `Описание:`}
+                  {lang === LangEnum.EN && `Description:`}
+                </span>
+
+                {lang === LangEnum.RU && project.descriptionRu}
+                {lang === LangEnum.EN && project.descriptionEn}
+              </li>
+            ) : null}
+
+            {project.purposeRu && project.purposeEn ? (
+              <li className={styles.element}>
+                <span className={styles.subtitle}>
+                  {lang === LangEnum.RU && `Цель проекта:`}
+                  {lang === LangEnum.EN && `Objective of the project:`}
+                </span>
+
+                {lang === LangEnum.RU && project.purposeRu}
+                {lang === LangEnum.EN && project.purposeEn}
+              </li>
+            ) : null}
+
             <li className={styles.element}>
-              <span className={styles.subtitle}>
-                {lang === LangEnum.RU && `Цель проекта:`}
-                {lang === LangEnum.EN && `Objective of the project:`}
-              </span>
-
-              {lang === LangEnum.RU && project.purposeRu}
-              {lang === LangEnum.EN && project.purposeEn}
-            </li>
-          ) : null}
-
-          <li className={styles.element}>
-            <a className={styles.link} href={project.gitLink} target="_blank">
-              Github
-            </a>
-          </li>
-
-          {project.pageLink ? (
-            <li className={styles.element}>
-              <a
-                className={styles.link}
-                href={project.pageLink}
-                target="_blank"
-              >
-                Git-pages
+              <a className={styles.link} href={project.gitLink} target="_blank">
+                Github
               </a>
             </li>
-          ) : null}
-        </ul>
+
+            {project.pageLink ? (
+              <li className={styles.element}>
+                <a
+                  className={styles.link}
+                  href={project.pageLink}
+                  target="_blank"
+                >
+                  Git-pages
+                </a>
+              </li>
+            ) : null}
+          </ul>
+        </div>
+
+        <SlideImages project={project} className={styles.images} />
       </div>
     </div>
   );
