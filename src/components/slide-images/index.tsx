c8455ca064ac1c02fdcd33bed6c8371950837546
Sergey Maximov <dev9092336062@gmail.com>
2024-01-17 14:49:20 +0300

    slide images

--- src/components/slide-images/index.tsx
+++ src/components/slide-images/index.tsx
@@ -4,8 +4,14 @@ import styles from "./styles.module.scss";
 import classNames from "classnames";
 
 const SlideImages: FC<ISlideImages> = ({ project, className }) => {
+  console.log(project.photos);
+
   return (
-    <div className={classNames(styles.element, className)}>{/* TODO */}</div>
+    <div className={classNames(styles.element, className)}>
+      {project.photos.map((el) => (
+        <img src={`./images/pets/${el}`} alt={el} className={styles.picture} />
+      ))}
+    </div>
   );
 };
 
