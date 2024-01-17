d81e8fe3285f9af8d15318fd24df2d22487c32ac
Sergey Maximov <dev9092336062@gmail.com>
2024-01-17 16:40:44 +0300

    slide images: animation

--- src/components/slide-images/index.tsx
+++ src/components/slide-images/index.tsx
@@ -1,15 +1,37 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { ISlideImages } from "./types";
 import styles from "./styles.module.scss";
 import classNames from "classnames";
 
 const SlideImages: FC<ISlideImages> = ({ project, className }) => {
-  console.log(project.photos);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    if (project.photos.length === 1) return;
+
+    const intervalFunction = () => {
+      setActiveIndex((pref) =>
+        pref < project.photos.length - 1 ? pref + 1 : 0,
+      );
+    };
+
+    const interval = setInterval(intervalFunction, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className={classNames(styles.element, className)}>
-      {project.photos.map((el) => (
-        <img src={`./images/pets/${el}`} alt={el} className={styles.picture} />
+      {project.photos.map((el, index) => (
+        <img
+          key={`${el} ${index}`}
+          src={`./images/pets/${el}`}
+          alt={`Image #-${index}`}
+          className={classNames(styles.picture, {
+            [styles.animated]:
+              project.photos.length > 1 && index === activeIndex,
+          })}
+        />
       ))}
     </div>
   );
